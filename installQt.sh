a3615bb29d14682014a4a9a40ed79f2b42d249f8
raulbocanegra <raul.bocanegra.algarra@gmail.com>
2016-09-26 20:19:45 +0200

    Recovered from and old repo

--- installQt.sh
+++ installQt.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 # Download and Install Qt
+wget http://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run
+chmod +x qt-opensource-linux-x64-5.7.0.run
+sudo ./qt-opensource-linux-x64-5.7.0.run
