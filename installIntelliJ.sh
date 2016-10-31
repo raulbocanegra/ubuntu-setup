75516013a9f44076999ebdfd62f4c1e5f5112770
Raul Bocanegra Algarra <raul.bocanegra.algarra@gmail.com>
2016-10-31 22:36:40 +0100

    intellij updated

--- installIntelliJ.sh
+++ installIntelliJ.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Download and Install Qt
-#wget https://d1opms6zj7jotq.cloudfront.net/idea/ideaIC-15.0.1.tar.gz
-tar vxzf ideaIC-15.0.1.tar.gz
+# Download and Install IntelliJ
+#wget https://download-cf.jetbrains.com/idea/ideaIC-2016.2.4.tar.gz
+tar vxzf ideaIC-2016.2.4.tar.gz
 sudo mv idea-IC-143.382.35 /opt/
